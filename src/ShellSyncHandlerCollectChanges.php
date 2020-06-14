c967fa4a07e0000191c9ec813fb2a4e170b2e103
Michael Stilkerich <ms@mike2k.de>
2020-06-14 18:06:04 +0200

    Add copyright headers

--- src/ShellSyncHandlerCollectChanges.php
+++ src/ShellSyncHandlerCollectChanges.php
@@ -1,5 +1,26 @@
 <?php
 
+/*
+ * davshell - Simple Shell to interact with (Card)DAV servers
+ *
+ * Copyright (C) 2020 Michael Stilkerich <ms@mike2k.de>
+ *
+ * This file is part of davshell.
+ *
+ * davshell is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * davshell is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with davshell.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 /**
  * Synchronization handler that collects the synchronization result to local variables for
  * later processing.
